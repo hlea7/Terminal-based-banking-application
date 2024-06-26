f92ceab5b786d2e0c4ec5391df03e626b855a01a
hlea7 <hladaj7@gmail.com>
2024-06-26 20:10:18 +0200

    Account.py

--- Account.py
+++ Account.py
@@ -9,45 +9,45 @@ class Account:
         
 
     def write_to_history(self, hist_dict):
-        pass 
-        # TODO:
-        # Comment and refine the code below so that the dictionary 
-        # from hist_dict is added to hist.json
-    
-        # self.file_manager 
+        # the dictionary from hist_dict is added to hist.json    
+        return self.file_manager.add_to_json(hist_dict, self.hist_file_path) 
 
     def deposit(self, amount):
-        pass
-        # TODO:
-        # implement the deposit process with all necessary checks
-        # amount must be a integer greater than 0
+        try:
+            amount = int(amount)
+            # amount must be a integer greater than 0
+            if amount > 0:
+                self.balance += amount
+                # in case of a positive outcome, write it to a JSON file
+                history_message = HistoryMessages.deposit("success", amount, self.balance)                
+            else:
+                # in case of a negative outcome, write to the JSON file
+                history_message = HistoryMessages.deposit("failure", amount, self.balance)
+                print('Invalid amount for deposit!')
+        except ValueError:            
+            history_message = HistoryMessages.deposit("failure", amount, self.balance)
+            print("Invalid amount for deposit!")   
+        self.write_to_history(history_message) 
         
-        # in case of a positive outcome, use this construct to write it to a JSON file
-
-        # history_message = HistoryMessages.deposit("success", amount, self.balance)
-        # self.write_to_history(history_message)
-
-        # in case of a negative outcome, use this construct to write to the JSON file
-            
-        # history_message = HistoryMessages.deposit("failure", amount, self.balance)
-        # self.write_to_history(history_message)
 
     def debit(self, amount):
-        pass
-        # TODO:
-        # implement account debits with all necessary checks
-        # amount must be a integer greater than 0
-        # if amount is greater than the amount in the account (insufficient funds) the operation should not work
-
-        # in case of positive outcome use this construct to write to JSON file
-
-        # history_message = HistoryMessages.debit("success", amount, self.balance)
-        # self.write_to_history(history_message)
-
-        # in case of a negative outcome, use this construct to write to a JSON file
-        
-        # history_message = HistoryMessages.debit("failure", amount, self.balance)
-        # self.write_to_history(history_message)
+        try:
+            amount = int(amount)
+            # amount must be a integer greater than 0
+            if amount > 0 and amount <= self.balance:
+                self.balance -= amount
+                # in case of positive outcome write to JSON file                
+                history_message = HistoryMessages.debit("success", amount, self.balance)
+            else:
+                # in case of a negative outcome, write to a JSON file
+                # if amount is greater than the amount in the account (insufficient funds) the operation should not work
+                history_message = HistoryMessages.debit("failure", amount, self.balance)
+                print("Invalid amount for debit!") 
+        except ValueError:
+            history_message = HistoryMessages.debit("failure", amount, self.balance)
+            print('Invalid amount for debit!')
+        self.write_to_history(history_message)
+    
 
     def get_balance(self):
         return self.balance
@@ -60,7 +60,12 @@ class Account:
         
 
     def get_history(self):
-        pass
-        # TODO:
-        # implement a process that returns transaction history line by line
-        # use the dict_to_string method to create a string from a dictionary
+        # returns transaction history line by line using the dict_to_string method to create a string from a dictionary
+        transaction_history_data = self.file_manager.read_json(self.hist_file_path)
+        transaction_history_lines = ""
+        for dict_data in transaction_history_data:
+            if dict_data == transaction_history_data[0]:
+                transaction_history_lines += f"-- {self.dict_to_string(dict_data)} --"
+            else:
+                transaction_history_lines += f"\n-- {self.dict_to_string(dict_data)} --"
+        return transaction_history_lines
